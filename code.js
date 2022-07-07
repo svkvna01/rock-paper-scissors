7063c4bf2a13972d1817f1ef58ed31710059f205
svkvna01 <sarah.barghouti26@gmail.com>
2022-07-08 00:19:20 +0200

    One round
    correction user input

--- code.js
+++ code.js
@@ -1,9 +1,85 @@
+//starts game
+function game () {
+    playRound();
+}
+
+//Array of choices
+const choices = ['rock', 'paper', 'scissors'];
+
+// random computer play
 function computerPlay () {
-    var items = ['Rock', 'Paper', 'Scissors'];
-    var item = items[Math.floor(Math.random()*items.length)];
-    console.log(item);
+    return choices[Math.floor(Math.random()*choices.length)];
 }
 
-function playRound (playerSelection, computerSelection) {
-
+// Player input
+function playerChoice() {
+    let input = prompt("Enter Rock, Paper or Scissors to play");
+    while (input == null) {
+        input = prompt("Enter Rock, Paper or Scissors to play");
+    }
+    input = input.toLowerCase();
+    let check = validInput(input);
+    while (check == false) {
+      input = prompt(
+        "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitilization doesnt matter"
+      );
+      while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+      }
+      input = input.toLowerCase();
+      check = validInput(input);
+    }
+    return input;
 }
+
+//check valid input
+function validInput(choice) {
+    if (choices.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//One play round
+function playRound () {
+    const playerSelection = playerChoice();
+    const computerSelection = computerPlay();
+
+    // Tie 
+    if (playerSelection === computerSelection) {
+       console.log("It's a tie! Play again!");
+
+     // Rock beats Scissors
+    }else if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            console.log("You win! Rock beats Scissors!");
+        } else {
+            console.log("You lose! Paper beats Rock!");
+        }
+    
+    // Paper beats Rock
+    }else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            console.log("You win! Paper beats Rock!");
+        } else {
+            console.log( "You lose! Scissors beats Paper!");
+        }
+    
+    // Scissors beat Paper
+    }else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            console.log("You win! Scissors beats Paper!");
+        } else {
+            console.log("You lose! Rock beats Scissors!");
+ 
+        }
+    }
+}
+
+
+
+
+
+
+
