aa9bb7a1aaf301319f7fc37ea212b3dfbc56aaa1
svkvna01 <sarah.barghouti26@gmail.com>
2022-07-07 22:33:36 +0200

    computerPlay function

---
+++ code.js
@@ -0,0 +1,9 @@
+function computerPlay () {
+    var items = ['Rock', 'Paper', 'Scissors'];
+    var item = items[Math.floor(Math.random()*items.length)];
+    console.log(item);
+}
+
+function playRound (playerSelection, computerSelection) {
+
+}
